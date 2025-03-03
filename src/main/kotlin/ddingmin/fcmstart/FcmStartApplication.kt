6c8177caa900c0fb103a293a3bb5c195ee4a8a7a
ddingmin <ddingmin.dev@gmail.com>
2025-03-03 14:58:10 +0900

    chore: init

---
+++ src/main/kotlin/ddingmin/fcmstart/FcmStartApplication.kt
@@ -0,0 +1,11 @@
+package ddingmin.fcmstart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FcmStartApplication
+
+fun main(args: Array<String>) {
+    runApplication<FcmStartApplication>(*args)
+}
---
+++ src/test/kotlin/ddingmin/fcmstart/FcmStartApplicationTests.kt
@@ -0,0 +1,13 @@
+package ddingmin.fcmstart
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FcmStartApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
